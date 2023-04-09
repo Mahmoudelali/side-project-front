fef5a8f63694195e909adcf6216896ea1f3f5c30
mahmoudElali <mahmud.elali98@gmail.com>
2023-04-09 22:53:02 +0300

    add loader for the website in case of data misidentified

---
+++ side-project-front/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/loader.css';
+
+const Loader = () => {
+	return (
+		<div className="loader-container">
+			<span className="loader"></span>
+		</div>
+	);
+};
+
+export default Loader;
